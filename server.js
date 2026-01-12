05d7f1154ed7b5fe1e094c5097e748628a81e5ef
Keytocoin <sutartoblaz@gmail.com>
2026-01-12 09:57:24 +0700

    Update server.js
    Signed-off-by: Keytocoin <sutartoblaz@gmail.com>

--- server.js
+++ server.js
@@ -131,4 +131,4 @@ ws.send(JSON.stringify({ type:"info", message:"Connected to KeytoCoin WS" }));
 });
 
 /* ================= START SERVER ================= */
-server.listen(PORT, ()=>console.log(📡 ⛓️ KeytoCoin server running at http://keytocoin.Global:${PORT}));
+server.listen(PORT, ()=>console.log(📡 ⛓️ KeytoCoin server running at http://keytocoin.global:${PORT}));
